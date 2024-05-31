4d2074b2d2bbcdb6fe1c9efe4256a68004c53c4b
jwanpark <77142334+jwanp@users.noreply.github.com>
2024-05-31 14:13:43 +0900

    feat: Java monacoEditor and editor component on main page (#7)

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,6 +1,16 @@
+
+import Topbar from '../components/Header';
+import JavaDiffEditor from '../components/JavaDiffEditor';
+import JavaEditor from '../components/JavaEditor';
 import ExecutionResult from '../components/ExecutionResult';
 
 function Main() {
-  return <ExecutionResult />;
+  return (
+    <div>
+      <JavaEditor />
+      <ExecutionResult />;
+    </div>
+  );
+
 }
 export default Main;
