6b1c32e8e0ff4b9c789bc2cd69b19e235711d6b9
redhean <48268986+redhean@users.noreply.github.com>
2024-06-04 22:06:53 +0900

    Backend pattern (#9)
    * README.md (#1)
    
    * flyway (#2)
    
    * api (#3)
    
    * enable test (#4)
    
    * db
    
    * feat: Add GreenPattern and test code
    
    ---------
    
    Co-authored-by: Kevin Roh <46668918+GramBoxer@users.noreply.github.com>

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+### 실행
+
+```
+$ ./gradlew build
+$ docker compose up -d --build
+```
--- backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class Demo6ApplicationTests {
+@ActiveProfiles("test")
+class BackendApplicationTests {
 
   @Test
   void contextLoads() {
