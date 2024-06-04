a77a0e11aae0dd85862039598a85f49a00717908
redhean <48268986+redhean@users.noreply.github.com>
2024-06-04 22:21:04 +0900

    Delete backend/src/main/java/com/example/demo/domain/TestRepository.java

--- backend/src/main/java/com/example/demo/domain/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
