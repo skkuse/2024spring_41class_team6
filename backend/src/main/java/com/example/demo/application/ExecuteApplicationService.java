0a095f71a92d2cfa9825d84e29f4217d5d07c11b
Kevin Roh <46668918+GramBoxer@users.noreply.github.com>
2024-06-05 13:57:51 +0900

    feat: Execute (#15)

---
+++ backend/src/main/java/com/example/demo/application/ExecuteApplicationService.java
@@ -0,0 +1,69 @@
+package com.example.demo.application;
+
+import com.example.demo.domain.ExecutionResult;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class ExecuteApplicationService {
+    public ExecutionResult run(String code) {
+        long cpu = 0, memory = 0, runtime = 0;
+        try {
+            Path tempDir = Files.createTempDirectory("java-code");
+
+            Path javaFile = Files.write(tempDir.resolve("Temp.java"), code.getBytes());
+
+            ClassPathResource scriptResource = new ClassPathResource("run.sh");
+            Path scriptFile = tempDir.resolve("run.sh");
+            Files.copy(scriptResource.getInputStream(), scriptFile, StandardCopyOption.REPLACE_EXISTING);
+
+            scriptFile.toFile().setExecutable(true);
+
+            ProcessBuilder processBuilder = new ProcessBuilder(scriptFile.toString()).directory(tempDir.toFile());
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+            int i = 0;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    switch (i) {
+                        case 0:
+                            cpu = Long.parseLong(line);
+                            i++;
+                            break;
+                        case 1:
+                            memory = Long.parseLong(line);
+                            i++;
+                            break;
+                        case 2:
+                            runtime = Long.parseLong(line);
+                            i++;
+                            break;
+                    }
+                    output.append(line).append("\n");
+                }
+            }
+
+            process.waitFor(10, TimeUnit.SECONDS);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new ExecutionResult(
+                0,
+                "OK",
+                runtime,
+                memory,
+                0 // TODO: calculate emmision
+        );
+    }
+}
