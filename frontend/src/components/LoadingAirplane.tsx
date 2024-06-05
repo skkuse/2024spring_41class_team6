71b5c89b59ae229b6c800fbb77c9a6bb70a8c6d0
ourgye <102244584+ourgye@users.noreply.github.com>
2024-06-05 14:45:25 +0900

    feat: add loading component

--- frontend/src/components/LoadingAirplane.tsx
+++ frontend/src/components/LoadingAirplane.tsx
@@ -1,5 +1,53 @@
+import styled, { keyframes } from 'styled-components';
+import { ReactComponent as AirplaneIcon } from '../icons/airplane.svg';
+import { transform } from 'typescript';
+
 function LoadingAirplane() {
-  return;
+  return (
+    <LoadingDotWrapper>
+      <LoadingDots />
+      <LoadingDots />
+      <LoadingDots />
+      <AirplaneIcon
+        fill="var(--primary-100)"
+        width={'128px'}
+        transform="rotate(90)"
+      />
+    </LoadingDotWrapper>
+  );
 }
+
+//style
+const bounceLoader = keyframes`
+  to {
+    opacity: 0.1;
+  }
+`;
+
+const LoadingDots = styled.div`
+  display: flex;
+`;
+
+const LoadingDotWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  ${LoadingDots} {
+    width: 16px;
+    height: 16px;
+    margin: 3px 6px;
+    border-radius: 50%;
+    background-color: var(--primary-100);
+    opacity: 1;
+    animation: ${bounceLoader} 0.6s infinite alternate;
+  }
+  ${LoadingDots}:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  ${LoadingDots}:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+`;
 
 export default LoadingAirplane;
