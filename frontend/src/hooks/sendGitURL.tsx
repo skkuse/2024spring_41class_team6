65c373a9c63320c4a4d89a9764f39c7970b19d2e
ourgye <102244584+ourgye@users.noreply.github.com>
2024-06-13 17:17:21 +0900

    feat: add sendGitURL hook

---
+++ frontend/src/hooks/sendGitURL.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// gitURL을 보내기 위한 인터페이스
+interface GitURL {
+  prurl: string;
+}
+
+export async function sendGitURL(code: string, gitURL: string): Promise<any> {
+  // post로 서버에 code랑 gitURL 보내기
+  try {
+    const response = await axios.post('http://localhost:8000/?  ', {
+      code: code,
+      gitURL: gitURL,
+    });
+
+    // reqeust 실패
+    if (response.status === 200) {
+      console.log(response.data);
+      return response.data;
+    }
+
+    // reqeust 성공
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
